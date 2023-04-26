42277f6013d9255bfe0feab6fa5132c89c70c093
Eleonora Nocentini Sköldebrink <112484819+Elli2022@users.noreply.github.com>
2023-04-26 09:29:23 +0200

    time and auth

---
+++ middlewares/auth.js
@@ -0,0 +1,8 @@
+function isAuthenticated(req, res, next) {
+    if (req.session.authenticated && req.session.username) {
+        req.isAuthenticated = true;
+    } else {
+        req.isAuthenticated = false;
+    }
+    next()
+}
